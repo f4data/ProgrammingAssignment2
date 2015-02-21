5e2a3c2361cd188d6937f8c0b2a04c9ce7483760
Fernando Garcia <fgarcor@gmail.com>
2015-02-21 12:26:10 +0100

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions provide the inverse of a square invertible matrix
+## e.g.: 
+## m <- matrix(c(1, 0, 5, 2, 1, 6, 3, 4, 0), 3, 3)
+## x <- makeCacheMatrix(m)
+## cacheSolve(x)
+## cacheSolve(x)
 
-## Write a short comment describing this function
-
+## Placeholder for a matrix and its inverse value
+## Input: original matrix
+## Output: none
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        
+        ## Method to store the original matrix
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        
+        ## Method to retrieve the original matrix
+        get <- function() x
+        
+        ## Method to store the inverse matrix
+        setinverse <- function(inverse) i <<- inverse
+        
+        ## Method to retrieve the stored inverse matrix
+        getinverse <- function() i
+        
+        ## List of attributes (methods) available
+        list(set = set, get = get, 
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Computes the inverse of a matrix. If it has already been cached retrieves the inverse value.
+## Input: makeCacheMatrix (m)
+## Output: inverse of matrix m
+## The matrix m must be a square invertible matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Check whether there is already an inverse matrix cached
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting chached data")
+                return(i)                
+        }
+        
+        ## If there is no cached inverse matrix, try to compute it
+        data <- x$get()
+        i <- try(solve(data, ...))
+        
+        ## Store the new inverse matrix
+        x$setinverse(i)
+        
+        # Print out the value
+        i
 }
